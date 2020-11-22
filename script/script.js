07d8485ef7f9f810d290b8f96321c45a9e6b6dd2
mum33n <59535561+mum33n@users.noreply.github.com>
2020-11-22 18:07:12 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var toggler=document.querySelector("#toggler");
+var toggle=document.querySelector(".nav-list");
+document.getElementById("toggler").addEventListener("click", function(){
+   toggle.classList.toggle("displayNav");
+})
